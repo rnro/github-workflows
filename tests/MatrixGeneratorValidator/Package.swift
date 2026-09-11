404cabec6c1acd0cb9ce810ec733403e6e88cf76
Rick Newton-Rogers <rnro@apple.com>
2026-09-11 15:58:51 -0400

    WIP package

--- tests/MatrixGeneratorValidator/Package.swift
+++ tests/MatrixGeneratorValidator/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
   name: "MatrixGeneratorValidator",
   targets: [
+    .executableTarget(name: "generate-matrix"),
     .target(name: "MatrixTestSupport"),
     .testTarget(
       name: "MatrixGeneratorTests",
